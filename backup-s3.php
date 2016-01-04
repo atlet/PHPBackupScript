3baa22b12eea2b8372a42173b5fb9017bd39d788
Andraž <atletek@gmail.com>
2016-01-04 13:17:37 +0000

    First commit.

---
+++ backup-s3.php
@@ -0,0 +1,86 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'config.php';
+
+use Aws\S3\S3Client;
+use Aws\Common\Credentials\Credentials;
+
+$credentials = new Credentials(ACCESS_KEY, SECRET_KEY);
+
+$errors = array();
+
+// Instantiate the S3 client with your AWS credentials
+$client = S3Client::factory(array(
+            'credentials' => $credentials
+        ));
+
+$client->createBucket(array('Bucket' => BUCKET));
+$client->waitUntil('BucketExists', array('Bucket' => BUCKET));
+
+$tmpDate = date("Y-m-d-H-i-s");
+$folder = $tmpDate . "/";
+
+foreach ($sqlTables as $value) {
+    $SQLFileName = "{$value}-{$tmpDate}.sql.gz";
+
+    $backupSqlFile = getcwd() . "/" . $SQLFileName;
+    $commandSQL = "mysqldump --add-drop-table --routines -u {$mySqlUsername} -p{$mySqlPassword}  {$value} | gzip > " . $backupSqlFile;
+    $ret = system($commandSQL);
+    
+    if (!empty($ret)) {
+        $errors[] = $ret;
+    }
+
+    $result = $client->putObject(array(
+        'Bucket' => BUCKET,
+        'Key' => $folder . $SQLFileName,
+        'SourceFile' => $backupSqlFile
+    ));
+
+    $client->waitUntil('ObjectExists', array(
+        'Bucket' => BUCKET,
+        'Key' => $folder . $SQLFileName
+    ));
+
+    unlink($backupSqlFile);
+}
+
+foreach ($directories as $key => $value) {
+    $directoryFileName = "{$key}-{$tmpDate}.sql.gz";
+    $backupDirectoryFileName = getcwd() . "/" . $directoryFileName;
+
+    $command = "tar -czf $backupDirectoryFileName -C {$value} {$key}";
+    $ret = system($command);
+    
+    if (!empty($ret)) {
+        $errors[] = $ret;
+    }
+
+    $result = $client->putObject(array(
+        'Bucket' => BUCKET,
+        'Key' => $folder . $directoryFileName,
+        'SourceFile' => $backupDirectoryFileName
+    ));
+
+    $client->waitUntil('ObjectExists',
+            array(
+        'Bucket' => BUCKET,
+        'Key' => $folder . $directoryFileName
+    ));
+
+    unlink($backupDirectoryFileName);
+}
+
+if (SEND_MAIL) {
+    $transport = Swift_MailTransport::newInstance();
+    $mailer = Swift_Mailer::newInstance($transport);
+
+    $newMail = Swift_Message::newInstance();
+    $newMail->setSubject("[" . BUCKET . "] Backup - " . date("d.m.Y H:i:s"));
+    $newMail->setFrom($from);
+    $newMail->setTo($to);
+    $newMail->setBody("Backup successfully created!\n\n" . print_r($errors, TRUE));
+
+    $result = $mailer->send($newMail);
+}
